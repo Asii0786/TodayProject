289d15e4853c1fad993c8119d0dc190c6ddb7b77
Asii0786 <asikasii0786@gmail.com>
2023-01-18 07:24:21 +0530

    asii push the code

--- src/org/dem/Sample.java
+++ src/org/dem/Sample.java
@@ -3,5 +3,12 @@ package org.dem;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("push");
+
+	System.out.println("yasar push the code");
+
+	System.out.println("push the code asik");
+	
+	System.out.println("asii push the code");
+	
 }
 }
